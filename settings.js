297bf4500495fd603582b4f6f947fed4834b30eb
UTHUMAN321 <kibuukauthuman321@gmail.com>
2025-03-20 12:50:22 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ const chalk = require('chalk')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
-global.SESSION_ID = process.env.SESSION_ID || 'Bellah~2z40RA7T#VRneuCjwFxSYHupVJSJHZDNmNDE_THLCNxjVd9jGWuw' 
+global.SESSION_ID = process.env.SESSION_ID || '' 
 //owmner v card
 global.ytname = "YT: GIDDYTENNOR" //ur yt chanel name
 global.socialm = "IG: @GIDDYTENNOR" //ur github or insta name
@@ -16,24 +16,24 @@ global.location = "Kenya" //ur location
 
 //new
 global.botname = process.env.BOT_NAME ||'𝐁𝐞𝐥𝐥𝐚𝐡 𝐗𝐦𝐝' //enter your  bot name here
-global.ownernumber = process.env.OWNER_NUMBER ||'254104245659' //ur owner number
+global.ownernumber = process.env.OWNER_NUMBER ||'256784670936' //ur owner number
 global.ownername = '© TENNOMODZ' //ur owner name
-global.websitex = "https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
-global.wagc = "https://chat.whatsapp.com/CzFlFQrkdzxFw0pxCBYM7H" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
+global.websitex = "https://whatsapp.com/channel/0029Vb57ZHh7IUYcNttXEB3y" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
+global.wagc = "https://whatsapp.com/channel/0029Vb57ZHh7IUYcNttXEB3y" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
 global.themeemoji = '🪀'
 global.wm = "GIDDY TENNOR"
-global.botscript = 'https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D' //'https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES' //script link
+global.botscript = 'https://whatsapp.com/channel/0029Vb57ZHh7IUYcNttXEB3y' //'https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES' //script link
 global.packname = process.env.PACK_NAME ||"BELLAH XMD" //enter your stickers author name here
-global.author = "Giddy-Tennor"
-global.creator = "254703726139@s.whatsapp.net"
+global.author = "Uthuman"
+global.creator = "256784670936@s.whatsapp.net"
 global.xprefix = process.env.BOT_PREFIX ||'.'
-global.premium = ["254703726139"] // Premium User
+global.premium = ["256784670936"] // Premium User
 global.hituet = 0
 
 //bot sett
 global.typemenu = process.env.TYPE_MENU || 'v2' // menu type 'v1' => 'v8' 
 global.typereply = process.env.TYPE_REPLY ||'v1' // reply type 'v1' => 'v3'
-global.autoblocknumber = process.env.AUTOBLOCK_NUMBER || '263,234' //set autoblock country code
+global.autoblocknumber = process.env.AUTOBLOCK_NUMBER || '263,1' //set autoblock country code
 global.antiforeignnumber = process.env.ANTIFOREIGN_NUMBER || '' //set anti foreign number country code
 global.welcome = process.env.WELCOME || 'false' //welcome/left in groups
 global.anticall = process.env.ANTI_CALL || 'false' //bot blocks user when called
